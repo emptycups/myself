2e03d29119af90d95114b9c71095f0dd11cb700d
weiwei.kang <weiwei.kang@huifu.com>
2018-12-07 10:50:27 +0800

    76789

--- Test4/com.test.www/Test.java
+++ Test4/com.test.www/Test.java
@@ -3,7 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		int a = 0;
 		int b= 0;
-	        int c =0;
+	    int c =0;
+	    int d=0000;
 		System.out.println("ÄãºÃ°¡");
 		System.out.println("ÄãºÃ°¡");
 	}
