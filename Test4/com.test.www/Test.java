eabf9b7e84cecc97f43b93fdf2fa2b764899ef4d
emptycups <42474260+emptycups@users.noreply.github.com>
2018-12-07 12:56:28 +0800

    Update Test.java

--- Test4/com.test.www/Test.java
+++ Test4/com.test.www/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 	public static void main(String[] args) {
 		int a = 0;
-		System.out.println("ÄãºÃ°¡");
+		System.out.println("ÄãºÃ°¡dadadadda");
 		System.out.println("ÄãºÃ°¡");
 		System.out.println("ÄãºÃ°¡");
 		System.out.println("ÄãºÃ°¡");
