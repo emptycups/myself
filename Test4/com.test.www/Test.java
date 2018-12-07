96879da4f9dbb28fabcd3410d56d8adf1bd515e6
emptycups <42474260+emptycups@users.noreply.github.com>
2018-12-07 10:38:12 +0800

    Update Test.java

--- Test4/com.test.www/Test.java
+++ Test4/com.test.www/Test.java
@@ -2,9 +2,6 @@
 public class Test {
 	public static void main(String[] args) {
 		int a = 0;
-		int b= 0;
-	        int c =0;
-		int d =0;
 		System.out.println("ÄãºÃ°¡");
 		System.out.println("ÄãºÃ°¡");
 	}
