cbce3733df481510e3aef27fccb73300672fb84c
kongbeizi <kong_beizi@sina.com>
2018-12-09 18:25:12 +0800

    eqeqdadad

--- Test4/src/AAA.java
+++ Test4/src/AAA.java
@@ -3,7 +3,8 @@ public class AAA {
 	public static void main(String[] args) {
 		System.out.println("yeysy");
 		System.out.println("yeysy");
-		int a = 0;
+		System.out.println("yeysy");
+		int b = 0;
 		System.out.println("yeysy");
 	}
 }
