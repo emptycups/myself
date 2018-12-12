18925b8824fb52e31a6c4712b4e85371f33fa2a0
emptycups <42474260+emptycups@users.noreply.github.com>
2018-12-12 21:02:01 +0800

    Update AAA.java

--- test1/src/com/test/www/AAA.java
+++ test1/src/com/test/www/AAA.java
@@ -4,6 +4,7 @@ public class AAA {
       int a = 10;
       System.out.print("dadad");
       System.out.print("dadad");
+       System.out.print("dadaddad");
       int c = 20;
       int c = 20;
 }
