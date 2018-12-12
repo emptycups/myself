9326d3b724c88dc3e4a2f70a834814df3c89cfe3
weiwei.kang <weiwei.kang@huifu.com>
2018-12-12 21:03:04 +0800

    1212

--- test1/src/com/test/www/AAA.java
+++ test1/src/com/test/www/AAA.java
@@ -4,6 +4,8 @@ public class AAA {
       int a = 10;
       System.out.print("dadad");
       System.out.print("dadad");
+      System.out.print("dadaddad");
+      System.out.print("dadaddsdsdsdd");
       int c = 20;
       int c = 20;
 }
