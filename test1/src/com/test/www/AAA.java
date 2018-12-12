394b64444c609dda777dd2026f33322ebfea8b71
emptycups <42474260+emptycups@users.noreply.github.com>
2018-12-12 20:59:08 +0800

    Update AAA.java

--- test1/src/com/test/www/AAA.java
+++ test1/src/com/test/www/AAA.java
@@ -3,6 +3,6 @@ package com.test.www;
 public class AAA {
       int a = 10;
       System.out.print("dadad");
-      System.out.print("dadad");
+      System.out.print("dadxccccad");
       int c = 20;
 }
